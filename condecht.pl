9f2acfaf5f4a6f62441d2b7b1f3ccf0cdf851f7a
Benedikt Heine <user@bebe-arch-lap.local>
2012-12-30 21:28:33 +0100

    before cleanup (backup now)

--- condecht.pl
+++ condecht.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 
 ##LOAD MODULES
-#use warnings;
+use warnings;
 #use strict;
 use Getopt::Long;
 use Pod::Usage;
@@ -19,12 +19,17 @@ my $host;
 
 ## DEFAULT VARIABLES
 my $config_main = "/etc/condecht";
-my $config_pkg = "packages.conf";
+##temp
+my $config_pkg = "packages.conf.new";
 ##END DEFAULT VARIABLES
 
 ##HOOK-Functions
 sub hook {
-	system "$main{path}host-$main{host}.d/$package.d/$_" if( -x "$main{path}host-$main{host}.d/$package.d/$_");
+	for $package (@pkgs){
+		if(-x $main{path} . $main{host} . "/" . $package . "/" . $_[0]){
+			system $main{path} . $main{host} . "/" . $package . "/" . $_[0];
+		}
+	}
 }
 ##END USED FUNCTIONS
 
@@ -32,32 +37,29 @@ sub hook {
 GetOptions(
 	##general options
 	"c|config=s" => \$config_main,
-	"h|host=s" => \$host,
-	"check" => sub { if(!$mode){ $mode = "cc"; } else { exit(1); }},
-	"help|h" => sub { pod2usage(2) },
+	"h|host=s"   => \$host,
+	"check"      => sub { if(!$mode){ $mode = "cc"; } else { exit(1); }},
+	"help|h"     => sub { pod2usage(1) },
 	##install/remove configs/packages
-	"pi=s@" => sub { if(!$mode){ $mode = "pi"; @pkgs = @_; shift(@pkgs); } else { exit(1); }},
-	"pr=s@" => sub { if(!$mode){ $mode = "pr"; @pkgs = @_; shift(@pkgs); } else { exit(1); }},
-	"ci=s@" => sub { if(!$mode){ $mode = "ci"; @pkgs = @_; shift(@pkgs); } else { exit(1); }},
-	"cr=s@" => sub { if(!$mode){ $mode = "cr"; @pkgs = @_; shift(@pkgs); } else { exit(1); }},
+	"pi=s@"      => sub { if(!$mode){ $mode = "pi"; @pkgs = @_; shift(@pkgs); } else { exit(1); }},
+	"pr=s@"      => sub { if(!$mode){ $mode = "pr"; @pkgs = @_; shift(@pkgs); } else { exit(1); }},
+	"ci=s@"      => sub { if(!$mode){ $mode = "ci"; @pkgs = @_; shift(@pkgs); } else { exit(1); }},
+	"cr=s@"      => sub { if(!$mode){ $mode = "cr"; @pkgs = @_; shift(@pkgs); } else { exit(1); }},
 	##list hosts/packages
-	"lh" => sub { if(!$mode){ $mode = "Lh"; } else { exit(1); }},
-	"lp" => sub { if(!$mode){ $mode = "Lp"; } else { exit(1); }},
+	"lp"         => sub { if(!$mode){ $mode = "lp"; } else { exit(1); }},
 	##updating the repo or the client
-	"Sc" => sub { if(!$mode){ $mode = "Sc"; } else { exit(1); }},
-	"Ss" => sub { if(!$mode){ $mode = "Sc"; } else { exit(1); }},
+	"Sc"         => sub { if(!$mode){ $mode = "sc"; } else { exit(1); }},
+	"Ss"         => sub { if(!$mode){ $mode = "ss"; } else { exit(1); }},
 );
 
 ##CHECK PARAMETERS
-	##EMPTY
+if(!defined $mode){
+	die "No mode specified!";
+}
 ##END CHECK PARAMETERS
 
 ##READ MAIN CONFIG ##
 my $cfg = Config::IniFiles->new(-file => $config_main) or die "@Config::IniFiles::errors";
-
-#check variables of hostfile
-#a check of the variables is necessary here
-
 if($cfg->SectionExists("main")){
 	for(("path", "backup", "host", "dist", "pkgINS", "pkgREM", "repServerUpd", "repClientUpd")){
 		if(defined $cfg->val("main", $_)){
@@ -101,131 +103,122 @@ my $pkg = Config::IniFiles->new(-file => $main{config_pkg}) or die "@Config::Ini
 ##END INIT PACKAGES CONFIG
 
 ##DO OTHER THINGS THAN DEPLOYING/REMOVING PACKAGES ##
-##list hosts
-if($mode eq "lh"){
-	for $host ($pkg->Groups){
-		print "$host\n";
-	}
-	exit(0);
-}
-##list packages
 if($mode eq "lp"){
-	for $member ($pkg->GroupMembers($main{host})){
-		print "$member\n";
+	for $package ($pkg->Groups){
+		print "$package\n";
 	}
 	exit(0);
 }
 #check config
 if($mode eq "cc"){
 	die "check config not implemented";
-	checkconfig();
+	#checkconfig();
 }
-if($mode eq "Sc"){
+if($mode eq "sc"){
 	#update client
 	die "update client not implemented yet";
 }
-if($mode eq "Ss"){
+if($mode eq "ss"){
 	die "update Server not implemented yet";
 }
 ##END DO OTHER THINGS THAN DEPLOYING/REMOVING PACKAGES ##
 
-#PAKETE AUSLESEN, WELCHE GEBRAUCHT WERDEN
+#READ PACKAGE CONFIGS
 for $package (@pkgs){
-	if($pkg->SectionExists("$main{host} $package")){
-		if($package eq $pkg->val("$main{host} $package", "pkg")){
-			hook("pre");
-	
-			if($mode eq "pi"){
-				hook("pre_pkg_install");
-	
-				for $dist ($pkg->val("$main{host} $package", "dist")){
-					if($dist =~ /^$main{dist}\s*:(.*)/){
-						@syspkgs = split(" ", $1);
-					}
-				}
-	
-				system $main{pkgINS} . @syspkgs;
-	
-				if($? != 0){
-					print "The packagemanager returned an error code. Continue installing packages? [y/N]";
-					unless(<STDIN> =~ /y/i){
-						die "Stopped while installing the packages. You may have to fix some errors.";
-					}
-				}
-	
-				hook("post_pkg_install");
+	unless($pkg->SectionExists("$package all")){
+		die "Section [$package all] missing in $main{config_pkg}";
+	}
+	unless($pkg->SectionExists("$package $main{host}")){
+		warn "Section [$package $main{host}] missing in $main{config_pkg}";
+	}
+
+	for $dist ($pkg->val("$package all", "dist"), $pkg->val("$package $main{host}", "dist")){
+		if($dist =~ /^$main{dist}\s*:(.*)/){
+			push @syspkgs, split(" ", $1);
 			}
+		}
+		
+	for $file ($pkg->val("$package all", "file"), $pkg->val("$package $main{host}", "file")){
+		($fdest,$ffile,$fmode,$fuser,$fgroup) = split(",", $file);
+		if($files{$fdest}){
+			print "Overwriting\n$files{$ffile}\nwith\n$file";
+		}
+		$files{$fdest} = join(",", ($fdest, "$main{path}$main{host}/$package/$ffile", $fmode, $fuser, $fgroup));
+	}
 	
-			if($mode eq "cr" || $mode eq "pr"){
-				hook("pre_confif_remove");
+}
 	
-				for $file ($pkg->val("$main{host} $package", "file")){
-					($fdest,$fdest) = split(",", $file);
-					if($main{backup}){
-						system "mv -v $fdest, $main{path}/backup/$fdest" or die "Backup failed: $!";
-					}
-					else {
-						system "rm -v $fdest";
-					}
-				}
-	
-				hook("post_config_remove");
-			}
-	
-			if($mode eq "pr"){
-				hook("pre_pkg_remove");
-	
-				for $dist ($pkg->val("$main{host} $package", "dist")){
-					if($dist =~ /^$main{dist}\s*:(.*)/){
-						@syspkgs = split(" ", $1);
-					}
-				}
-	
-				system $main{pkgREM} . @syspkgs;
-	
-				if($? != 0){
-					print "The packagemanager returned an error code. Continue removing packages? [y/N]";
-					unless(<STDIN> =~ /y/i){
-						die "Stopped while removing the packages. You may have to fix some errors.";
-					}
-				}
-	
-				hook("post_pkg_remove");
-			}
-	
-			if($mode eq "ci" || $mode eq "pi"){
-				hook("pre_config_install");
-				
-				for $file ($pkg->val("$main{host} $package", "file")){
-					my ($ffile,$fdest,$fmode,$fowner,$fgroup) = split(",", $file);
-					$ffile = $main{path} . "host-" . $main{host} . ".d/" . $package . ".d/" . $ffile;
-	
-					system "cp -v $ffile $fdest";
-					system "chown -v $fowner:$fgroup $fdest";
-					system "chmod -v $fmode $fdest";
-	
-					##Alternative 
-					##It uses the internal Perl chmod and chown routines
-					##a little bit faster, but it is easier to add verbose output with system "ch...
-					#my ($fuid,$fuid,$fuid) = getpwnam($fowner) or die "$fowner not in passwd file";
-					#my ($fgid,$fgid,$fgid) = getgrnam($fgroup) or die "$fgroup not passwd file";
-					#chmod oct($fmode), $fdest;
-					#chown $fuid, $fguid, $fdest;
-				}
-	
-				hook("post_config_install");
-			}
-	
-			hook("post");
+hook("pre");
+
+if($mode eq "pi"){
+	hook("pre_pkg_install");
+
+	system $main{pkgINS} . join(" ", @syspkgs);
+
+	if($? != 0){
+		print "The packagemanager returned an error code. Continue installing configfiles? [y/N]";
+		unless(<STDIN> =~ /y/i){
+			die "Stopped while installing the packages. You may have to fix some errors.";
+		}
+	}
+
+	hook("post_pkg_install");
+}
+
+if($mode eq "cr" || $mode eq "pr"){
+	hook("pre_config_remove");
+
+	for $file (values %files){
+		($fdest) = split(",", $file);
+
+		if($main{backup}){
+
+			my $fdest2 = $fdest;
+			$fdest2 =~ s/^\///;
+			$fdest2 =~ s/\//-/g;
+
+			system "mv -v $fdest $main{path}backup/$fdest2";
 		}
 		else {
-			die "The value of pkg in the section [$main{host} $package] is not $package!";
+			system "rm -v $fdest";
 		}
 	}
-	else {
-		die "The section [$main{host} $package] is not defined in the file $main{config_pkg}!";
-	}
+	
+	hook("post_config_remove");
 }
+	
+if($mode eq "pr"){
+	hook("pre_pkg_remove");
+
+	system $main{pkgREM} . @syspkgs;
+
+	if($? != 0){
+		print "The packagemanager returned an error code. Continue removing configfiles? [y/N]";
+		unless(<STDIN> =~ /y/i){
+			die "Stopped while removing the packages. You may have to fix some errors.";
+		}
+	}
+	
+	hook("post_pkg_remove");
+}
+	
+if($mode eq "ci" || $mode eq "pi"){
+	hook("pre_config_install");
+	
+	for $file (values %files){
+		my ($fdest,$ffile,$fmode,$fowner,$fgroup) = split(",", $file);
+		
+		system "cp -v $ffile $fdest";
+		system "chown -v $fowner:$fgroup $fdest";
+		system "chmod -v $fmode $fdest";
+	}
+	
+	hook("post_config_install");
+}
+	
+hook("post");
+
+exit(0);
 
 __END__
 =head1 NAME
