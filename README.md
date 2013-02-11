91c5a10834d70ce3431f94adde30f4ece6abe0a6
Benedikt Heine <bebehei@gmail.com>
2013-02-11 05:12:15 +0100

    added License Information

--- README.md
+++ README.md
@@ -127,7 +127,11 @@ There are a few variables, which are important:
 
 LICENSE
 -------
-Condecht is free software, you can distribute it and modify it under the terms of the GPL.
+Copyright 2013 by Benedikt Heine
+Condecht is free software, you can distribute it and modify it under the terms of the GPL. You'll find it in the file LICENSE or at http://www.gnu.org/licenses/.
+
+# LICENSE of the ./lib directory
+The ./lib directory contains the Config::IniFile module, which is distributed under the same terms of perl (GPL). The copyright of the whole ./lib directory is at its different owners.
 
 TODO
 ----
--- condecht.pl
+++ condecht.pl
@@ -1,14 +1,33 @@
 #!/usr/bin/env perl
 
+#
+#   Copyright (C) 2013, Benedikt Heine
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#   You'll find the GPL in the LICENSE file.
+#
+
 ##LOAD MODULES
 use warnings;
 #use strict;
+use lib "./lib";
+use Config::IniFiles;
+##standard modules
 use Getopt::Long;
 use Pod::Usage;
 use File::Path;
-#use Config::IniFiles or
-use lib "./lib";
-use Config::IniFiles;
 use File::Copy;
 ##END LOAD MODULES
 
